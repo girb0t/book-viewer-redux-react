f2d3d2611ad9bec729a2299220b66a9f9f2c6829
Pei Xiong Liu <peixiong.liu@gmail.com>
2016-01-29 23:50:21 -0500

    Add root and book reducers

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+  return [
+    {title: 'Javascript: The Good Parts' },
+    {title: 'James and the Giant Peach'},
+    {title: 'Practical Object-Oriented Design in Ruby'},
+    {title: 'Animorphs'},
+  ];
+}
