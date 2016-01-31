38a9e5f544f38579bfcae7c99fb0e039500856fd
Pei Xiong Liu <peixiong.liu@gmail.com>
2016-01-31 10:27:52 -0500

    Hook BookDetail up to state

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function () {
   return [
-    {title: 'Javascript: The Good Parts' },
-    {title: 'James and the Giant Peach'},
-    {title: 'Practical Object-Oriented Design in Ruby'},
-    {title: 'Animorphs'},
+    {title: 'Javascript: The Good Parts', pages: 101 },
+    {title: 'James and the Giant Peach', pages: 330 },
+    {title: 'Practical Object-Oriented Design in Ruby', pages: 1},
+    {title: 'Animorphs', pages: 20 },
   ];
 }
