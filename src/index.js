bb46fe8acfde69b356329c64604e8308e5d6ffe0
Pei Xiong Liu <pliu@fiksu.com>
2016-12-07 21:43:26 -0500

    Add redux devtools support

--- src/index.js
+++ src/index.js
@@ -6,10 +6,13 @@ import { createStore, applyMiddleware } from 'redux';
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const createStoreWithMiddleware = createStore(reducers, composeEnhancers(
+  applyMiddleware()
+));
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={createStoreWithMiddleware}>
     <App />
   </Provider>
   , document.querySelector('.container'));
